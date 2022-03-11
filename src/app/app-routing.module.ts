6124a6f05fa58e79d97a8538ba0b6d8d0e2ecd4d
itaitã Araujo <itaitan2@gmail.com>
2022-03-11 20:03:13 -0300

    rota para tecnico List

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { NavComponent } from './components/nav/nav.component';
 import { componentFactoryName } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [{
   path: '',  component: NavComponent, children: [
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'tecnicos', component: TecnicoListComponent}
   ]
 }
 
