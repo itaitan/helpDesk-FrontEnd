1cedeb759d6346f28ce6db28793766404fa4e40c
itaitã Araujo <itaitan2@gmail.com>
2022-03-29 15:39:21 -0300

    Criação de um novo T

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,9 @@
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 import { FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Tecnico } from 'src/app/models/tecnicos';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,19 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoCreateComponent implements OnInit {
 
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: '',
+  }
+
+
   nome: FormControl = new FormControl(null, Validators.minLength(3))
   cpf: FormControl = new FormControl(null, Validators.required)
   email: FormControl = new FormControl(null, Validators.email)
   senha: FormControl = new FormControl(null, Validators.minLength(3))
 
-  constructor() { }
+  constructor( private service: TecnicoService,
+    private tost: ToastrService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+
+
+  create():void{
+    this.service.create(this.tecnico).subscribe(resposta => {
+      this.tost.success('Tecnico cadastrado com sucesso', 'Cadastro');
+      this.router.navigate(['tecnicos'])
+
+    }, ex =>{
+      if(ex.error.errors){
+        ex.error.errors.forEach(element => {
+          this.tost.error(element.message)
+        });
+      }else{
+        this.tost.error(ex.error.message)
+      }
+    })
+  }
+
+  addPerfil(perfil: any){
+
+    if (this.tecnico.perfis.includes(perfil)){
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1)
+
+    }else{
+      this.tecnico.perfis.push(perfil)
+    }
+
   }
 
   validaCampos():boolean{
     return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
   }
-
 
 }
