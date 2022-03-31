0bba76bb05627dea98b1959c2d63a9dd0d4caf63
itaitã Araujo <itaitan2@gmail.com>
2022-03-31 19:50:21 -0300

    Criando o componente update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {
   path: '',  component: NavComponent, canActivate:[AuthGuard], children: [
-    {path: 'home', component: HomeComponent},
-    {path: 'tecnicos', component: TecnicoListComponent},
-    {path: 'tecnicos/create', component: TecnicoCreateComponent}
+    { path: 'home', component: HomeComponent },
+    { path: 'tecnicos', component: TecnicoListComponent },
+    { path: 'tecnicos/create', component: TecnicoCreateComponent },
+    { path: 'tecnicos/update/:id', component: TecnicoUpdateComponent }
   ]
 }
 
