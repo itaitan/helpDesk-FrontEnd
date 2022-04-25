87ff8426910dbd392e0f2ba0a0771a30e06f120d
itaitã Araujo <itaitan2@gmail.com>
2022-04-25 20:53:03 -0300

    Update finalizado

--- src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
+++ src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -1,5 +1,4 @@
-
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { TecnicoService } from 'src/app/services/tecnico.service';
 import { FormControl, Validators } from '@angular/forms';
@@ -8,7 +7,7 @@ import { Tecnico } from 'src/app/models/tecnicos';
 @Component({
   selector: 'app-tecnico-update',
   templateUrl: './tecnico-update.component.html',
-  styleUrls: ['./tecnico-update.component.scss']
+  styleUrls: ['./tecnico-update.component.scss'],
 })
 export class TecnicoUpdateComponent implements OnInit {
   tecnico: Tecnico = {
@@ -19,52 +18,61 @@ export class TecnicoUpdateComponent implements OnInit {
     senha: '',
     perfis: [],
     dataCriacao: '',
-  }
+  };
 
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+  cpf: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  nome: FormControl = new FormControl(null, Validators.minLength(3))
-  cpf: FormControl = new FormControl(null, Validators.required)
-  email: FormControl = new FormControl(null, Validators.email)
-  senha: FormControl = new FormControl(null, Validators.minLength(3))
-
-  constructor( private service: TecnicoService,
+  constructor(
+    private service: TecnicoService,
     private tost: ToastrService,
-    private router: Router) { }
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.tecnico.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
   }
 
+  findById(): void {
+    this.service.findById(this.tecnico.id).subscribe((resposta) => {
+      resposta.perfis = [];
+      this.tecnico = resposta;
+    });
+  }
 
-
-  update():void{
-    this.service.create(this.tecnico).subscribe(resposta => {
-      this.tost.success('Tecnico cadastrado com sucesso', 'Cadastro');
-      this.router.navigate(['tecnicos'])
-
-    }, ex =>{
-      if(ex.error.errors){
-        ex.error.errors.forEach(element => {
-          this.tost.error(element.message)
-        });
-      }else{
-        this.tost.error(ex.error.message)
+  update(): void {
+    this.service.update(this.tecnico).subscribe(
+      (resposta) => {
+        this.tost.success('Tecnico modificado com sucesso', 'Cadastro');
+        this.router.navigate(['tecnicos']);
+      },
+      (ex) => {
+        if (ex.error.errors) {
+          ex.error.errors.forEach((element) => {
+            this.tost.error(element.message);
+          });
+        } else {
+          this.tost.error(ex.error.message);
+        }
       }
-    })
+    );
   }
 
-  addPerfil(perfil: any){
-
-    if (this.tecnico.perfis.includes(perfil)){
-      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1)
-
-    }else{
-      this.tecnico.perfis.push(perfil)
+  addPerfil(perfil: any) {
+    if (this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
     }
-
   }
 
-  validaCampos():boolean{
-    return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
+  validaCampos(): boolean {
+    return (
+      this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
+    );
   }
-
 }
--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -5,18 +5,27 @@ import { API_CONFIG } from '../config/api.config';
 import { Tecnico } from '../models/tecnicos';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TecnicoService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  findall(): Observable<Tecnico[]>{
-    return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`)
+  findById(id: any): Observable<Tecnico> {
+    return this.http.get<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos/${id}`);
   }
 
-  create(tecnico: Tecnico ): Observable<Tecnico>{
+  findall(): Observable<Tecnico[]> {
+    return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`);
+  }
+
+  create(tecnico: Tecnico): Observable<Tecnico> {
     return this.http.post<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos`, tecnico);
   }
 
+  update(tecnico: Tecnico): Observable<Tecnico> {
+    return this.http.put<Tecnico>(
+      `${API_CONFIG.baseUrl}/tecnicos/${tecnico.id}`,
+      tecnico
+    );
+  }
 }
