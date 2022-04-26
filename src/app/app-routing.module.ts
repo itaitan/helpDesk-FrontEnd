488c2359f6a5d7d5613a847da91a5ede083c2d3a
itaitã Araujo <itaitan2@gmail.com>
2022-04-26 19:46:19 -0300

    Implementação delete Tecnico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,22 +8,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
+import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
+  { path: 'login', component: LoginComponent },
   {
-  path: '',  component: NavComponent, canActivate:[AuthGuard], children: [
-    { path: 'home', component: HomeComponent },
-    { path: 'tecnicos', component: TecnicoListComponent },
-    { path: 'tecnicos/create', component: TecnicoCreateComponent },
-    { path: 'tecnicos/update/:id', component: TecnicoUpdateComponent }
-  ]
-}
-
+    path: '',
+    component: NavComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'tecnicos', component: TecnicoListComponent },
+      { path: 'tecnicos/create', component: TecnicoCreateComponent },
+      { path: 'tecnicos/update/:id', component: TecnicoUpdateComponent },
+      { path: 'tecnicos/delete/:id', component: TecnicoDeleteComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
+++ src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -1,15 +1,60 @@
+import { FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Tecnico } from 'src/app/models/tecnicos';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tecnico-delete',
   templateUrl: './tecnico-delete.component.html',
-  styleUrls: ['./tecnico-delete.component.scss']
+  styleUrls: ['./tecnico-delete.component.scss'],
 })
 export class TecnicoDeleteComponent implements OnInit {
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: '',
+  };
 
-  constructor() { }
+  constructor(
+    private service: TecnicoService,
+    private tost: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.tecnico.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
   }
 
+  findById(): void {
+    this.service.findById(this.tecnico.id).subscribe((resposta) => {
+      resposta.perfis = [];
+      this.tecnico = resposta;
+    });
+  }
+
+  delete(): void {
+    this.service.delete(this.tecnico.id).subscribe(
+      (resposta) => {
+        this.tost.success('Tecnico detado com sucesso', 'Cadastro');
+        this.router.navigate(['tecnicos']);
+      },
+      (ex) => {
+        if (ex.error.errors) {
+          ex.error.errors.forEach((element) => {
+            this.tost.error(element.message);
+          });
+        } else {
+          this.tost.error(ex.error.message);
+        }
+      }
+    );
+  }
 }
