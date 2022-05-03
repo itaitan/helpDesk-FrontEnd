3d62a9467fce04536bb85c750252a9c622494e00
itaitã Araujo <itaitan2@gmail.com>
2022-05-02 22:06:37 -0300

    RadioButton para list chamado

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,24 +6,24 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
-
-  constructor(private router: Router,
-     private authenticate: AuthService,
-     private toast: ToastrService) {
-
-   }
+  constructor(
+    private router: Router,
+    private authenticate: AuthService,
+    private toast: ToastrService
+  ) {}
 
   ngOnInit(): void {
-    this.router.navigate(['tecnicos'])
+    this.router.navigate(['chamados']);
   }
 
-  logout(){
+  logout() {
     this.router.navigate(['login']);
     this.authenticate.logout();
-    this.toast.info('Logout realizado com sucesso', 'LogOut', {timeOut: 7000})
+    this.toast.info('Logout realizado com sucesso', 'LogOut', {
+      timeOut: 7000,
+    });
   }
-
 }
